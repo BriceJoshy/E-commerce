37155abb5da9ec188a2b14828d69df26b59e9ecf
BRICE JOSHY <bricejoshy99@gmail.com>
2025-02-24 03:42:34 +0530

    Reusable Heading

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/common/custom_appbar.dart';
 import 'package:e_commerce/common/custom_container.dart';
+import 'package:e_commerce/common/heading.dart';
 import 'package:e_commerce/constants/constants.dart';
 import 'package:e_commerce/views/home/widgets/category_list.dart';
 import 'package:flutter/material.dart';
@@ -16,8 +17,22 @@ class HomePage extends StatelessWidget {
           preferredSize: Size.fromHeight(130.h), child: const CustomAppbar()),
       body: SafeArea(
         child: CustomContainer(
-          containerContent: const Column(
-            children: [CategoryList()],
+          containerContent: Column(
+            children: [
+              const CategoryList(),
+              Heading(
+                text: "Nearby Restaurants",
+                onTap: () {},
+              ),
+              Heading(
+                text: "Try Something New",
+                onTap: () {},
+              ),
+              Heading(
+                text: "Food Closer to You",
+                onTap: () {},
+              ),
+            ],
           ),
         ),
       ),

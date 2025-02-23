5ad216dc34bfdf7825f7532d5d19535be436e44c
BRICE JOSHY <bricejoshy99@gmail.com>
2025-02-24 00:04:12 +0530

    Initial commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+String googleApiKey = '';
+
+const kPrimary = Color(0xFF673AB7); // Deep Purple
+const kPrimaryLight = Color(0xFFD1C4E9); // Soft Purple
+const kSecondary = Color(0xFFFF5722); // Bright Orange
+const kSecondaryLight = Color(0xFFFFCCBC); // Light Orange
+const kTertiary = Color(0xFF4CAF50); // Green Accent
+const kGray = Color(0xFF9E9E9E); // Neutral Gray
+const kGrayLight = Color(0xFFE0E0E0); // Light Gray
+const kLightWhite = Color(0xFFF5F5F5); // Soft White
+const kWhite = Color(0xFFFFFFFF); // Pure White
+const kDark = Color(0xFF212121); // Dark Gray
+const kRed = Color(0xFFD32F2F); // Strong Red
+const kOffWhite = Color(0xFFFAFAFA); // Off White
+
+double height = 825.h;
+double width = 375.w;
+
+const String appBaseUrl = "http://localhost:6003";
